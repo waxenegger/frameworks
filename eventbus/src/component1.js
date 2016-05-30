d6c3fad2077d0a4bc54c74c2981bf8ca2193f86f
Harald Waxenegger <harald.waxenegger@gmail.com>
2016-05-30 20:08:40 +1000

    intentation change

--- eventbus/src/component1.js
+++ eventbus/src/component1.js
@@ -1,41 +1,44 @@
 var Component1 = function(web_glue, image_id) {
-	this.web_glue = web_glue;
-	this.web_glue.subscribe(ome.glue.EVENTS.IMAGE_CHANGE,
-		function(data, uid, time) {
-			this.init(data['id']);
-		}, this);
-		this.init(image_id);
+    this.web_glue = web_glue;
+    this.web_glue.subscribe(ome.glue.EVENTS.IMAGE_CHANGE, function(data, uid,
+            time) {
+        this.init(data['id']);
+    }, this);
+    this.init(image_id);
 };
 
 Component1.prototype.init = function(id) {
-	this.web_glue.request(
-		{url : "https://demo.openmicroscopy.org/webgateway/imgData/" + id,
-			dataType : "jsonp",
-			context : this,
-			success: Component1.prototype.populateWidget.bind(this)
-		});
+    this.web_glue.request({
+        url : "https://demo.openmicroscopy.org/webgateway/imgData/" + id,
+        dataType : "jsonp",
+        context : this,
+        success : Component1.prototype.populateWidget.bind(this)
+    });
 }
 
 Component1.prototype.populateWidget = function(data, what, whatElse) {
-		$("#component1").html("");
-		for (var c=0;c<data.channels.length;c++) {
-			var chan = data.channels[c];
-			var addhtml = '<input type="checkbox" id="channel_'
-							+ chan.label + '" class="channels" value="'
-							+ c + '"' + (chan.active ? "checked='checked'" : "")
-							+ '>' + chan.label + '</input><br>';
-			$("#component1").append(addhtml);
-			$("#channel_" + chan.label).on("change", function() {
-				var selected = [];
-				var channels = $(".channels");
-				channels.each(function( index ) {
-						if (this.checked=== true)
-							selected.push(parseInt(this.value));
-				});
-			this.web_glue.publish(ome.glue.EVENTS.IMAGE_DIMENSION_CHANGE,
-				 { id: data.id, dim: "c", value: selected});
-		}.bind(this));
-	}
+    $("#component1").html("");
+    for (var c = 0; c < data.channels.length; c++) {
+        var chan = data.channels[c];
+        var addhtml = '<input type="checkbox" id="channel_' + chan.label
+            + '" class="channels" value="' + c + '"'
+            + (chan.active ? "checked='checked'" : "") + '>' + chan.label
+            + '</input><br>';
+        $("#component1").append(addhtml);
+        $("#channel_" + chan.label).on("change", function() {
+            var selected = [];
+            var channels = $(".channels");
+            channels.each(function(index) {
+                if (this.checked === true)
+                    selected.push(parseInt(this.value));
+            });
+            this.web_glue.publish(ome.glue.EVENTS.IMAGE_DIMENSION_CHANGE, {
+                id : data.id,
+                dim : "c",
+                value : selected
+            });
+        }.bind(this));
+    }
 }
 
 module.exports = Component1;
--- eventbus/src/component2.js
+++ eventbus/src/component2.js
@@ -1,35 +1,37 @@
 var Component2 = function(web_glue, image_id) {
-	this.web_glue = web_glue;
-	this.web_glue.subscribe(ome.glue.EVENTS.IMAGE_CHANGE,
-		function(data, uid, time) {
-			this.init(data['id']);
-		}, this);
-		this.init(image_id);
+    this.web_glue = web_glue;
+    this.web_glue.subscribe(ome.glue.EVENTS.IMAGE_CHANGE, function(data, uid,
+            time) {
+        this.init(data['id']);
+    }, this);
+    this.init(image_id);
 }
 
 Component2.prototype.init = function(id) {
-	this.web_glue.request(
-		{url : "https://demo.openmicroscopy.org/webgateway/imgData/" + id,
-			dataType : "jsonp",
-			context : this,
-			success: Component2.prototype.populateWidget.bind(this)
-		});
+    this.web_glue.request({
+        url : "https://demo.openmicroscopy.org/webgateway/imgData/" + id,
+        dataType : "jsonp",
+        context : this,
+        success : Component2.prototype.populateWidget.bind(this)
+    });
 }
 
 Component2.prototype.populateWidget = function(data, what, whatElse) {
-	if (data.size.t <= 1) {
-		$("#component2").html("only one time point");
-		return;
-	}
+    if (data.size.t <= 1) {
+        $("#component2").html("only one time point");
+        return;
+    }
 
-	var html = '<input id="time" type="range" step="1"'
-		+ 'min="0" value="0"' + 'max="' + (data.size.t-1) + '"/>';
-	$("#component2").html(html);
-		$("#time").on("change", function() {
-		this.web_glue.publish(
-			ome.glue.EVENTS.IMAGE_DIMENSION_CHANGE,
-			{ id: data.id, dim: "t", value: [parseInt($("#time").val())]});
-	}.bind(this));
+    var html = '<input id="time" type="range" step="1"' + 'min="0" value="0"'
+            + 'max="' + (data.size.t - 1) + '"/>';
+    $("#component2").html(html);
+    $("#time").on("change", function() {
+        this.web_glue.publish(ome.glue.EVENTS.IMAGE_DIMENSION_CHANGE, {
+            id : data.id,
+            dim : "t",
+            value : [ parseInt($("#time").val()) ]
+        });
+    }.bind(this));
 }
 
 module.exports = Component2;
