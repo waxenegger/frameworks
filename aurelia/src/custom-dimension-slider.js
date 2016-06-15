bbf0081208806dc8400a173e8bd0f001e260c44a
Harald Waxenegger <harald.waxenegger@gmail.com>
2016-06-15 17:43:30 +1000

    forgot to add files

---
+++ aurelia/src/custom-dimension-slider.js
@@ -0,0 +1,77 @@
+import {inject} from 'aurelia-framework';
+import Configuration from './configuration.js';
+import {EVENTS} from './events.js';
+import {EventAggregator} from 'aurelia-event-aggregator';
+import {customElement} from 'aurelia-framework';
+import {slider} from 'jquery-ui';
+
+@customElement('custom-dimension-slider')
+@inject(Configuration, EventAggregator, Element)
+export default class CustomDimensionSlider {
+    subscriptions = [];
+    sub_list = [
+        [EVENTS.FORCE_UPDATE, (image_id) => this.forceUpdate(image_id)]
+    ];
+    constructor(config, eventbus, element) {
+        this.config = config;
+        this.eventbus =  eventbus;
+        this.element = element;
+        this.dim = this.element.id === 'time' ? 't' : 'z';
+        this.elSelector = "#" + this.element.id + " [name='dim']";
+
+        this.subscribeToEvents();
+
+    }
+
+    attached() {
+        $(this.elSelector).addClass(this.dim === 'z' ? "height100" : "");
+        $(this.elSelector).hide();
+        $(this.elSelector).slider({
+            orientation: this.dim === 'z' ? "vertical" : "horizontal",
+            min: 0, max: 1, step: 1, value: 0,
+            change: (event, ui) => this.onChange(ui.value)
+        });
+    }
+
+    detached() {
+        $(this.elSelector).slider( "destroy" );
+    }
+
+    subscribeToEvents() {
+        this.unsubscribe();
+        this.sub_list.map(
+            (value) => this.subscriptions.push(
+                this.eventbus.subscribe(value[0], value[1])
+            ));
+    }
+
+    unsubscribe() {
+        if (this.subscriptions.length === 0) return;
+        while (this.subscriptions.length > 0)
+            (this.subscriptions.pop())();
+    }
+
+    onChange(value) {
+        if (value === this.config.dimensions[this.dim]) return;
+
+        this.config.dimensions[this.dim] = parseInt(value);
+        this.eventbus.publish(
+            EVENTS.DIMENSION_CHANGE,
+            {dim: this.dim, value: [this.config.dimensions[this.dim]]});
+    }
+
+    forceUpdate() {
+        $(this.elSelector).slider(
+            "option", "value", this.config.dimensions[this.dim]);
+        $(this.elSelector).slider(
+            "option", "max", this.config.dimensions['max_' + this.dim]-1);
+        if (this.config.dimensions[this.dim] === 1)
+            $(this.elSelector).slider("option", "disabled", true);
+        else $(this.elSelector).slider("option", "disabled", false);
+        $(this.elSelector).show();
+    }
+
+    unbind() {
+        this.unsubscribe();
+    }
+}
---
+++ aurelia/src/custom-viewer.js
@@ -0,0 +1,90 @@
+import {inject} from 'aurelia-framework';
+import Configuration from './configuration.js';
+import {EVENTS} from './events.js';
+import {EventAggregator} from 'aurelia-event-aggregator';
+import {customElement} from 'aurelia-framework';
+
+import '../styles/viewer.css';
+import {ol3} from '../libs/ome-viewer-1.0.js';
+
+@customElement('custom-viewer')
+@inject(Configuration, EventAggregator)
+export default class CustomViewer {
+    lastShowRegions = false;
+    subscriptions = [];
+    sub_list = [
+        [EVENTS.IMAGE_CHANGE, (imageid) => this.viewer.changeToImage(imageid)],
+        [EVENTS.FORCE_UPDATE, () => this.forceUpdate()],
+        [EVENTS.REGIONS_VISIBILITY, (flag) => this.updateRegionsVisibility(flag)],
+        [EVENTS.DIMENSION_CHANGE, (data, event) =>
+            this.viewer.setDimensionIndex.apply(
+                this.viewer, [data.dim].concat(data.value))]
+    ];
+    constructor(config, eventbus) {
+        this.config = config;
+        this.eventbus =  eventbus;
+
+        this.subscribeToEvents();
+        this.viewer =
+            new ol3.Viewer(config.image_id, {server : config.server });
+        this.updateRegionsVisibility(this.config.show_regions);
+    }
+
+    subscribeToEvents() {
+        this.unsubscribe();
+        this.sub_list.map(
+            (value) => this.subscriptions.push(
+                this.eventbus.subscribe(value[0], value[1])
+            ));
+    }
+
+    unsubscribe() {
+        if (this.subscriptions.length === 0) return;
+        while (this.subscriptions.length > 0)
+            (this.subscriptions.pop())();
+    }
+
+    updateRegionsVisibility(value) {
+        if (value) {
+            this.viewer.addRegions();
+            this.viewer.setRegionsVisibility(true, []);
+            this.viewer.setRegionsModes(
+                [ol3.REGIONS_MODE.TRANSLATE,
+                ol3.REGIONS_MODE.MODIFY]);
+            this.viewer.enableRegionsContextMenu(true);
+        } else {
+            this.viewer.enableRegionsContextMenu(false);
+            this.viewer.setRegionsModes(
+            [ol3.REGIONS_MODE.DEFAULT]);
+            this.viewer.setRegionsVisibility(false, []);
+        }
+    }
+
+    unbind() {
+        this.unsubscribe();
+        if (this.viewer) {
+            this.viewer.destroyViewer();
+            this.viewer = null;
+        }
+    }
+
+    forceUpdate()  {
+        if (this.config.show_regions != this.lastShowRegions) {
+            this.updateRegionsVisibility(this.config.show_regions);
+            this.lastShowRegions = this.config.show_regions;
+        }
+
+        var presentZ = this.viewer.getDimensionIndex('z');
+        var presentT = this.viewer.getDimensionIndex('t');
+        var newZ = this.config.dimensions.z;
+        var newT = this.config.dimensions.t;
+
+        if (presentZ !== newZ)
+            this.viewer.setDimensionIndex.apply(
+                this.viewer, ['z'].concat([newZ]));
+
+        if (presentT !== newT)
+        this.viewer.setDimensionIndex.apply(
+            this.viewer, ['t'].concat([newT]))
+    }
+}
